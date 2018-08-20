dbac8cda057ca612cc721174889b8463c00e63ed
vimm0 <vimmrana0@gmail.com>
2018-08-20 23:50:15 +0545

    Optimize image

---
+++ sw.js
@@ -0,0 +1,14 @@
+// var urlsToCache = [];
+//
+// var CACHE_NAME = 'james-ives-cache-v1';
+//
+// self.addEventListener('install', function(event) {
+//   // Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function(cache) {
+//         console.log('Opened cache');
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
